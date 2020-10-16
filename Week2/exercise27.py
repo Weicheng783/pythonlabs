9227edf96c67dd63409d7dfb4c9ab6a32aede092
g28738wa <weicheng.ao@student.manchester.ac.uk>
2020-10-16 17:06:14 +0100

    exercise27

---
+++ Week2/exercise27.py
@@ -0,0 +1,12 @@
+
+num1 = input("Please enter the first number: ")
+print ("The first number you entered " + num1)
+
+num2 = input("Please enter the second number: ")
+print ("The second number you entered " + num2)
+
+print('The sum of them is '+str(int(num1)+int(num2)))
+print('The product of them is '+str(int(num1)*int(num2)))
+print('The ratio of them is '+str(int(num1)//int(num2)))
+print('The modulus of them is '+str(int(num1)%int(num2)))
+print('The exponentiation of them is '+str(int(num1)**int(num2)))
