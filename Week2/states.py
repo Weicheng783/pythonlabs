d677345f906edc9db371c40904d07243c75f0d4f
g28738wa <weicheng.ao@student.manchester.ac.uk>
2020-10-16 15:06:06 +0100

    states

---
+++ Week2/states.py
@@ -0,0 +1,11 @@
+def printState():
+    print('Red Light is '+redLight)
+    print('Yellow Light is '+yellowLight)
+    print('Green Light is '+greenLight)
+
+redLight='on'
+yellowLight='off'
+greenLight='off'
+
+printState()
+
