12d32ea95f6264e2df41e1666e6f0455dcd88d08
g28738wa <weicheng.ao@student.manchester.ac.uk>
2020-10-17 14:53:46 +0100

    Added some more functionality to the game

--- Week2/adventuregame.py
+++ Week2/adventuregame.py
@@ -1,20 +1,38 @@
-# ---My Test Based Adventure Game--- #
+# ---My Test Based Adventure Game---
+import time, os
 
-playerName = input('What is your name? ')
+print('Loading. . . .')
+time.sleep(2)
 
-print('\n\nHello'+ playerName)
+print('Welcome to my game'+os.getlogin()+'\nPlease choose a character: ')
+
+
+#playerName = input('What is your name? ')
+#print('\n\nHello'+ playerName)
 
 #print('1.Knight')
 #print('2.Warrior')
 #print('3.Wizard')
 characterList=['Knight','Warrior','Wizard']
+
+# Added a while loop to print out characterList
+i=0
+while i < len(characterList):
+    print(str(i+1)+'.'+characterList[i])
+    i += 1
+
+
 # since the character list could grow with new characters,
 # better not to use a literal string of characters in the output.
-print('\nChoose a character:',*characterList,sep='\n')
+#print('\nChoose a character:',*characterList,sep='\n')
+
+#character=input('Type in your character number: ')
+#selectedCharacter=characterList[int(character)-1]
+
+character=int(input('Type in your character number: '))
+selectedCharacter=characterList[character-1]
 
 
 
-character=input('Type in your character number: ')
-
-print('You chose:' + characterList[int(character)-1])
+print('You chose:' + selectedCharacter)
 
